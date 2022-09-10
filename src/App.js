4efbff6775ff1223d316ddf704d7d028b26709f2
Chao Zhou <Czhou0k@gmail.com>
2022-09-10 14:47:03 -0400

    change nav link to display inline

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
       <h1>ConvertOh</h1>
       <h2>A Conversion Calulator</h2>
       <div className={style.border}>
-        <nav>
-          <li>Hello</li>
-          <li>Temp</li>
+        <nav className={style.navLink}>
+          <li className={style.link}>Temp</li>
+          <li className={style.link}>Currency</li>
+          <li className={style.link}>Measurement</li>
         </nav>
       </div>
     </>
