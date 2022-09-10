74a8e4c8d1a137da2665922c161965876b28371a
Chao Zhou <Czhou0k@gmail.com>
2022-09-10 13:36:02 -0400

    added a border

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import style from './App.module.css';
 
 function App() {
-  return <div className='App'>Hello</div>;
+  return <div className={style.border}></div>;
 }
 
 export default App;
