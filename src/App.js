9e8cd5882db2e14ba7da756c6ffc4e0e8b56239f
Chao Zhou <Czhou0k@gmail.com>
2022-09-10 13:42:46 -0400

    added text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import { useEffect, useState } from 'react';
 import style from './App.module.css';
 
 function App() {
-  return <div className={style.border}></div>;
+  return (
+    <>
+      <h1>ConvertOh</h1>
+      <h2>A Conversion Calulator</h2>
+      <div className={style.border}></div>
+    </>
+  );
 }
 
 export default App;
