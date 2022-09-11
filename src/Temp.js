5d0527ef2d0b3cbe5cb278ec6c3a53b8145d0fef
Chao Zhou <Czhou0k@gmail.com>
2022-09-11 19:19:52 -0400

    save point to making temp conversion more interactive

--- src/Temp.js
+++ src/Temp.js
@@ -2,23 +2,29 @@ import React, { useState } from 'react';
 
 function Temp() {
   const [temp, setTemp] = useState({ f: null, c: null });
-  const [fromTemp, setFromTemp] = useState(null);
-  const [toTemp, setToTemp] = useState(null);
-
-  const setC = (e) =>
-    setTemp({ c: e.target.value, f: (e.target.value * 9) / 5 + 32 });
-  const setF = (e) =>
-    setTemp({ c: ((e.target.value - 32) * 5) / 9, f: e.target.value });
+  const setC = (e) => {
+    setTemp({
+      c: e.target.value,
+      f: ((+e.target.value * 9) / 5 + 32).toFixed(2),
+    });
+  };
+  const setF = (e) => {
+    setTemp({
+      c: (((+e.target.value - 32) * 5) / 9).toFixed(2),
+      f: e.target.value,
+    });
+  };
   return (
     <>
       <h1>Temp</h1>
-      <input type='number' value={temp.f} onChange={setC}></input>
+      <input type='number' onChange={setF}></input>
 
       <select>
-        <option>Fahrenheit</option>
+        <option value={temp.f}>Fahrenheit</option>
+        <option value={temp.c}>Celsius</option>
       </select>
 
-      <input type='number' value={temp.c} onChange={setF}></input>
+      <input type='number' value={temp.c} onChange={setC}></input>
 
       <select>
         <option>Celsius</option>
