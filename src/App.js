be01a2d8f3daf346b5839e392018054963959a4b
Chao Zhou <Czhou0k@gmail.com>
2022-09-11 10:35:07 -0400

    moving css from app to home

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import style from './App.module.css';
 
 function App() {
-  return (
-    <>
-      <h1>ConvertOh</h1>
-      <h2>A Conversion Calulator</h2>
-      <div className={style.border}>
-        <nav className={style.navLink}>
-          <li className={style.link}>Temp</li>
-          <li className={style.link}>Currency</li>
-          <li className={style.link}>Measurement</li>
-        </nav>
-      </div>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
