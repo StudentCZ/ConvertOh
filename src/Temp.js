9d5cdb66ae3d5b5ec2a5b4c139b24213f02a9c5f
Chao Zhou <Czhou0k@gmail.com>
2022-09-11 13:26:16 -0400

    added input and drop down

--- src/Temp.js
+++ src/Temp.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const temps = ['Fahrenheit', 'Celcius'];
 
 function Temp() {
   return (
     <>
       <h1>Temp</h1>
+      <input></input>
+      {temps.map((ele) => (
+        <select key='id'>
+          <option>{ele}</option>
+        </select>
+      ))}
     </>
   );
 }
