d7cbe64c152d4fd825f6ebfcd2b24b2995322cc3
Chao Zhou <Czhou0k@gmail.com>
2022-09-11 10:52:20 -0400

    rerouted to home page and added Temp component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
