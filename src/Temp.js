dd0827c53fc28843dcf0ef0a81e402f3656c51c9
Chao Zhou <Czhou0k@gmail.com>
2022-09-11 13:58:30 -0400

    added option and input for temp component

--- src/Temp.js
+++ src/Temp.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 
-const temps = ['Fahrenheit', 'Celcius'];
-
 function Temp() {
   return (
     <>
       <h1>Temp</h1>
       <input></input>
-      {temps.map((ele) => (
-        <select key='id'>
-          <option>{ele}</option>
-        </select>
-      ))}
+
+      <select>
+        <option>Fahrenhit</option>
+        <option>Celcius</option>
+      </select>
     </>
   );
 }
