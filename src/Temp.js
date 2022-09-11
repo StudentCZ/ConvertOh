6a61cd5664aa47cf78a55f387d33f69d30857ae8
Chao Zhou <Czhou0k@gmail.com>
2022-09-11 18:59:29 -0400

    got the temp conversion somewhat working

--- src/Temp.js
+++ src/Temp.js
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
 
 function Temp() {
+  const [temp, setTemp] = useState({ f: null, c: null });
+  const [fromTemp, setFromTemp] = useState(null);
+  const [toTemp, setToTemp] = useState(null);
+
+  const setC = (e) =>
+    setTemp({ c: e.target.value, f: (e.target.value * 9) / 5 + 32 });
+  const setF = (e) =>
+    setTemp({ c: ((e.target.value - 32) * 5) / 9, f: e.target.value });
   return (
     <>
       <h1>Temp</h1>
-      <input></input>
+      <input type='number' value={temp.f} onChange={setC}></input>
 
       <select>
         <option>Fahrenheit</option>
+      </select>
+
+      <input type='number' value={temp.c} onChange={setF}></input>
+
+      <select>
         <option>Celsius</option>
       </select>
     </>
