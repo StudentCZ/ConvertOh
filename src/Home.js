ba8c9ba3548f81e81cac98f3a323c721955a0649
Chao Zhou <Czhou0k@gmail.com>
2022-09-11 10:34:02 -0400

    added a css file for home

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1>ConvertOh</h1>
+      <h2>A Conversion Calulator</h2>
+      <div className={style.border}>
+        <nav className={style.navLink}>
+          <li className={style.link}>Temp</li>
+          <li className={style.link}>Currency</li>
+          <li className={style.link}>Measurement</li>
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default Home;
