aeb464f376339e14a5e0a848be5ebe48d2919fc5
Chao Zhou <Czhou0k@gmail.com>
2022-09-11 11:16:48 -0400

    added currency component and css file

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { useState } from 'react';
 import style from './Home.module.css';
-import { Link } from 'react-router-dom';
 import Temp from './Temp';
 
 function Home() {
   const [showTemp, setShowTemp] = useState(false);
-  const onClick = () => setShowTemp(true);
+  const TempOn = () => setShowTemp(true);
+  const [showCurrency, setShowCurrency] = useState(false);
+  const CurrencyOn = () => setShowCurrency(true);
+
   return (
     <>
       <h1>ConvertOh</h1>
       <h2>A Conversion Calulator</h2>
       <div className={style.border}>
         <nav className={style.navLink}>
-          <li className={style.link} onClick={onClick}>
+          <li className={style.link} onClick={TempOn}>
             Temp
           </li>
-          <li className={style.link}>Currency</li>
+          <li className={style.link} onClick={CurrencyOn}>
+            Currency
+          </li>
           <li className={style.link}>Measurement</li>
         </nav>
         {showTemp ? <Temp /> : null}
+        {showCurrency ? <Currency /> : null}
       </div>
     </>
   );
--- src/Temp.js
+++ src/Temp.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Home from './Home';
 
-const Temp = () => {
+function Temp() {
   return (
     <>
       <h1>Hello</h1>
     </>
   );
-};
+}
 
 export default Temp;
