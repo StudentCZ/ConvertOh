cb634c5539339c725e6f69e9e52c5586335e9380
Chao Zhou <Czhou0k@gmail.com>
2022-09-14 04:13:33 -0400

    added css to input boxes

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './CurrencyRow.module.css';
 
 function CurrencyRow() {
   return (
     <div>
-      <input type='number' />
+      <input type='number' className={style.input} />
       <select>
         <option value='test'>test</option>
       </select>
