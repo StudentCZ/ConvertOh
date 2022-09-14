a5f24cb681c5a450a09c7b2366cf0bb734b29952
Chao Zhou <Czhou0k@gmail.com>
2022-09-14 05:13:44 -0400

    added a from and to currency state

--- src/Currency.js
+++ src/Currency.js
@@ -6,13 +6,17 @@ const EXCHANGE_URL = `https://api.exchangerate.host/latest`;
 
 function Currency() {
   const [currencyOption, setCurrencyOption] = useState([]);
-  console.log(currencyOption);
+  const [fromCurrency, setFromCurrency] = useState(null);
+  const [toCurrency, setToCurrency] = useState(null);
 
   useEffect(() => {
     fetch(EXCHANGE_URL)
       .then((res) => res.json())
       .then((data) => {
+        const firstCurrency = Object.keys(data.rates)[0];
         setCurrencyOption([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
       });
   }, []);
 
