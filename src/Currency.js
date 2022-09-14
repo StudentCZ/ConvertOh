5a6bce440c9b26429ccdb74f895f8030fcd9a0d9
Chao Zhou <Czhou0k@gmail.com>
2022-09-14 04:53:03 -0400

    added api

--- src/Currency.js
+++ src/Currency.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CurrencyRow from './CurrencyRow';
+import style from './Currency.module.css';
+
+const EXCHANGE_URL = `https://api.exchangerate.host/latest`;
 
 function Currency() {
+  useEffect(() => {
+    fetch(EXCHANGE_URL)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <>
       <h1>Convert</h1>
       <CurrencyRow />
-      <div>=</div>
+      <div className={style.equals}>=</div>
       <CurrencyRow />
     </>
   );
